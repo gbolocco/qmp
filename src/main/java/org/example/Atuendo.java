d318aabbe0f1c79d8eeee5aa0f756ea83b59357f
gbolocco <gbolocco@frba.utn.edu.ar>
2025-04-16 12:58:03 -0300

    initial commit

---
+++ src/main/java/org/example/Atuendo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Atuendo {
+  public List<Prenda> prendas;
+
+  public void addPrenda(Prenda unaPrenda) {
+    this.prendas.add(unaPrenda);
+  }
+}
---
+++ src/main/java/org/example/Prenda.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class Prenda {
+
+  public enum Material { TELA, ALGODON, CUERO, GABARDINA }
+
+  public enum Color { ROJO, VERDE, AZUL, AMARILLO, VIOLETA, NEGRO, BLANCO, MARRON, NARANJA, ROSA, GRIS, NO_COLOR }
+
+  public enum Tipo {                //Los enums son objetos que pueden tener comportamiento y atributos!!!!
+
+    CAMISA(Categoria.PARTE_SUPERIOR),
+    PANTALON(Categoria.PARTE_INFERIOR),
+    ZAPATO(Categoria.CALZADO),
+    RELOJ(Categoria.ACCESORIO);
+
+    // Atributo del enum
+    private Categoria categoria;
+
+    // Constructor
+    Tipo(Categoria categoria) {
+          this.categoria = categoria;
+    }
+
+    // Getter
+    public Categoria getCategoria() {return categoria;}
+
+    // Enum anidad(categorias)
+    public enum Categoria {CALZADO, PARTE_SUPERIOR, PARTE_INFERIOR, ACCESORIO;}
+  }
+
+  private Tipo tipo;
+  private Material material;
+  private Color colorPrimario;
+  private Color colorSecundario;
+
+  //constructor
+  public Prenda(Tipo tipo, Color colorPrimario, Color colorSecundario, Material material) {
+    this.tipo = tipo;
+    this.colorPrimario = colorPrimario;
+    this.colorSecundario = colorSecundario;
+    this.material = material;
+  }
+
+  public Tipo getTipo() {
+    return tipo;
+  }
+
+}
+
+// >> Prenda camisa = new Prenda(Tipo.CAMISA, Color.ROJO,null, Material.TELA);
